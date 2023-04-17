b1b4e24d0d87edf682d1ded093e22b48803b3bf3
thrudr <ariel.oliveiraesilva@gmail.com>
2023-04-17 16:16:51 +0200

    exercise-completed

--- Geometric/ClassGeometric.cs
+++ Geometric/ClassGeometric.cs
@@ -6,8 +6,34 @@ using System.Threading.Tasks;
 
 namespace Geometric
 {
-    internal class ClassGeometric
+    public class rectangle
     {
+        // FEATURES
 
+        public int baseRectangle;
+        public int heightRectangle;
+
+        // METHODS
+
+        public int areaCalculation()
+        {
+            int areaRectangle = baseRectangle * heightRectangle;
+            return areaRectangle;
+        }
+
+        public int perimeterCalculation()
+        {
+            int perimeterRectangle = (baseRectangle + heightRectangle) * 2;
+            return perimeterRectangle;
+        }
+
+        public void printRectangle()
+        {
+            Console.WriteLine("BASE: \t \t" + baseRectangle + " cm");
+            Console.WriteLine("HEIGHT: \t" + heightRectangle + " cm");
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("AREA: \t \t" + areaCalculation() + " cm2");
+            Console.WriteLine("PERIMETER: \t" + perimeterCalculation() + " cm");
+        }
     }
 }
--- Geometric/Program.cs
+++ Geometric/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Geometric;
+
 Console.WriteLine("GEOMETRIC");
+
+rectangle userRectangle = new rectangle();
+
+Console.WriteLine("Inserire dimensione base rettangolo:");
+int userBase = int.Parse(Console.ReadLine());
+userRectangle.baseRectangle = userBase;
+
+Console.WriteLine("Inserire dimensione altezza rettangolo:");
+int userHeight = int.Parse(Console.ReadLine());
+userRectangle.heightRectangle = userHeight;
+
+Console.WriteLine("-------------------Rectangle------------------------");
+userRectangle.printRectangle();
+Console.WriteLine("----------------------------------------------------");
